2ed02469dcdb07bde992121f6e16aa2704f10d7a
Zannatul Maria <zannatulfarzana363@gmail.com>
2026-03-07 00:28:29 +0600

    modified

--- 3 array_properties.py
+++ 3 array_properties.py
@@ -3,6 +3,13 @@ import numpy as np
 c = np.array([[1, 2, 3],
               [4, 5, 6]])
 
-print("Shape:",c.shape)
+print("Shape:", c.shape)
 print("Size:", c.size)
 print("Dimension:", c.ndim)
+
+# compatibility
+a1 = np.array([1, 4, 7])
+a2 = np.array([2, 5, 9, 6])
+a3 = np.array([3, 6, 5])
+print("\nCompatibility shapes: ", a1.shape == a2.shape) 
+print("Compatibility shapes: ", a1.shape == a3.shape) 
